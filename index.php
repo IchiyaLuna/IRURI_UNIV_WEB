54233e7edd280eca5cc3515fe1ee32d72ebc7195
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-02 21:58:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -580,7 +580,7 @@
 
 
         <!-- Alert Modal -->
-        <div class="modal fade" id="no-input" tabindex="-1" aria-hidden="true">
+        <div class="alert-modal modal fade" id="no-input" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
