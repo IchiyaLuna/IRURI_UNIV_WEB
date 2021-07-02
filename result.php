661de2577e8d33d46ac3bea231abbeea86a6bcfd
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-02 20:58:10 +0900

    result render

---
+++ result.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <?php
+            
+        echo $_POST['gender-radio'];
+        echo $_POST['type-radio'];
+        
+        echo $_POST['first-korean'];    
+        echo $_POST['first-math'];
+        echo $_POST['first-english'];
+        echo $_POST['first-history'];
+        echo $_POST['first-social'];
+        echo $_POST['first-science'];
+
+        echo $_POST['second-korean'];
+        echo $_POST['second-math'];
+        echo $_POST['second-english'];
+        echo $_POST['second-history'];
+        echo $_POST['second-social'];
+        echo $_POST['second-science'];
+
+        echo $_POST['third-korean'];
+        echo $_POST['third-math'];
+        echo $_POST['third-english'];
+        echo $_POST['third-history'];
+        echo $_POST['third-social'];
+        echo $_POST['third-science'];
+        ?>
+    </body>
+</html>
