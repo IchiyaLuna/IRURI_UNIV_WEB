de93c30bbad19a8d602a881199dc78d9aa912f7b
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-03 01:31:34 +0900

    result table responsible

--- result.php
+++ result.php
@@ -161,138 +161,139 @@ $simple_avg /= $count;
                     <p class="lead">학생의 기본 정보와 내신 성적을 입력하시면 학원의 자체 데이터를 기반으로 수시 전형 추천 대학을 보여드립니다.</p>
 
                     <div class="card card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
 
-                        <table class="table table-bordered">
-
-                            <thead>
-                                <tr>
-                                    <th scope="col">성별</th>
-                                    <th scope="col">계열</th>
-                                    <th scope="col">단순 평균</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><?php echo $gender; ?></td>
-                                    <td><?php echo $type; ?></td>
-                                    <td><?php echo round($simple_avg, 2); ?></td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3">
-                                        <table class="table mb-0 table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col">합격예측</th>
-                                                    <th scope="col">대학</th>
-                                                    <th scope="col">내 환산등급</th>
-                                                    <th scope="col">상세 정보</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr class="table-danger">
-                                                    <td>위험</td>
-                                                    <td>서울대</td>
-                                                    <td>1.3</td>
-                                                    <td>상세</td>
-                                                </tr>
-                                                <tr class="table-warning">
-                                                    <td>불안</td>
-                                                    <td>연세대(서울)</td>
-                                                    <td>1.2</td>
-                                                    <td>상세</td>
-                                                </tr>
-                                                <tr class="table-success">
-                                                    <td>가능</td>
-                                                    <td>고려대(서울)</td>
-                                                    <td>1.2</td>
-                                                    <td>상세</td>
-                                                </tr>
-                                                <tr class="table-primary">
-                                                    <td>안정</td>
-                                                    <td>한양대</td>
-                                                    <td>1.2</td>
-                                                    <td>상세</td>
-                                                </tr>
-                                                <tr class="table-primary">
-                                                    <td>안정</td>
-                                                    <td>성균관대</td>
-                                                    <td>1.2</td>
-                                                    <td>상세</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                    </div>
-                </div>
-
-                <div class="content-sidebar col-md-4 d-none d-md-block sticky-md-top">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5>등급 안내</h5>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">내신 등급 비율 안내</h5>
-                            <p class="card-text">비율을 참고하여 등급을 입력해주세요.</p>
-                            <table class="table table-hover">
                                 <thead>
                                     <tr>
-                                        <th scope="col">등급</th>
-                                        <th scope="col">누적비율</th>
-                                        <th scope="col">등급비율</th>
+                                        <th scope="col">성별</th>
+                                        <th scope="col">계열</th>
+                                        <th scope="col">단순 평균</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <th class="grades-label" scope="row">1</th>
-                                        <td>~ 4%</td>
-                                        <td>4%</td>
+                                        <td><?php echo $gender; ?></td>
+                                        <td><?php echo $type; ?></td>
+                                        <td><?php echo round($simple_avg, 2); ?></td>
                                     </tr>
                                     <tr>
-                                        <th class="grades-label" scope="row">2</th>
-                                        <td>~ 11%</td>
-                                        <td>7%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">3</th>
-                                        <td>~ 23%</td>
-                                        <td>12%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">4</th>
-                                        <td>~ 40%</td>
-                                        <td>17%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">5</th>
-                                        <td>~ 60%</td>
-                                        <td>20%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">6</th>
-                                        <td>~ 77%</td>
-                                        <td>17%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">7</th>
-                                        <td>~ 89%</td>
-                                        <td>12%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">8</th>
-                                        <td>~ 96%</td>
-                                        <td>7%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">9</th>
-                                        <td>~ 100%</td>
-                                        <td>4%</td>
+                                        <td colspan="3">
+                                            <table class="table mb-0 table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">합격예측</th>
+                                                        <th scope="col">대학</th>
+                                                        <th scope="col">내 환산등급</th>
+                                                        <th scope="col">상세 정보</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr class="table-danger">
+                                                        <td>위험</td>
+                                                        <td>서울대</td>
+                                                        <td>1.3</td>
+                                                        <td>상세</td>
+                                                    </tr>
+                                                    <tr class="table-warning">
+                                                        <td>불안</td>
+                                                        <td>연세대(서울)</td>
+                                                        <td>1.2</td>
+                                                        <td>상세</td>
+                                                    </tr>
+                                                    <tr class="table-success">
+                                                        <td>가능</td>
+                                                        <td>고려대(서울)</td>
+                                                        <td>1.2</td>
+                                                        <td>상세</td>
+                                                    </tr>
+                                                    <tr class="table-primary">
+                                                        <td>안정</td>
+                                                        <td>한양대</td>
+                                                        <td>1.2</td>
+                                                        <td>상세</td>
+                                                    </tr>
+                                                    <tr class="table-primary">
+                                                        <td>안정</td>
+                                                        <td>성균관대</td>
+                                                        <td>1.2</td>
+                                                        <td>상세</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
+
+                        </div>
+                    </div>
+
+                    <div class="content-sidebar col-md-4 d-none d-md-block sticky-md-top">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5>등급 안내</h5>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">내신 등급 비율 안내</h5>
+                                <p class="card-text">비율을 참고하여 등급을 입력해주세요.</p>
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">등급</th>
+                                            <th scope="col">누적비율</th>
+                                            <th scope="col">등급비율</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th class="grades-label" scope="row">1</th>
+                                            <td>~ 4%</td>
+                                            <td>4%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">2</th>
+                                            <td>~ 11%</td>
+                                            <td>7%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">3</th>
+                                            <td>~ 23%</td>
+                                            <td>12%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">4</th>
+                                            <td>~ 40%</td>
+                                            <td>17%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">5</th>
+                                            <td>~ 60%</td>
+                                            <td>20%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">6</th>
+                                            <td>~ 77%</td>
+                                            <td>17%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">7</th>
+                                            <td>~ 89%</td>
+                                            <td>12%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">8</th>
+                                            <td>~ 96%</td>
+                                            <td>7%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">9</th>
+                                            <td>~ 100%</td>
+                                            <td>4%</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
