005989338e97c978c99c4718faad530e14bd3eba
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-03 17:29:34 +0900

    babo

--- sqload.php
+++ sqload.php
@@ -2,8 +2,9 @@
 $hostname = "localhost";
 $user = "iruri";
 $password = "test123";
+$dbname = "sushi_db";
 
-$database = mysqli_connect($hostname, $user, $password);
+$database = mysqli_connect($hostname, $user, $password, $dbname);
 if (!$database) {
     die("데이터베이스 연결 실패 [ERROR] : ".mysqli_connect_error());
 }
