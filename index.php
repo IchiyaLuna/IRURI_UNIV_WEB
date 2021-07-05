bb5bf27c83fdf3cc5dabebf0698310d445c25533
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-05 19:59:00 +0900

    lock zoom

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <meta name="description" content="">
     <meta name="author" content="Ichiya Luna">
     <title>UNDER DEV - Main Page</title>
