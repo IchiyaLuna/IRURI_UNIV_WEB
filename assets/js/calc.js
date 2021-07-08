6278a480665fb5d7d54b6d27689f573f57ecb8c4
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 15:37:35 +0900

    Update calc.js

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -9,13 +9,11 @@ $(function () {
         NewEntry.find('input').val('');
         DynamicForm.find('.subject-input:not(:last) .btn-add').removeClass('btn-add').addClass('btn-remove');
         DynamicForm.find('.subject-input:not(:last) .btn-remove').removeClass('btn-outline-success').addClass('btn-outline-danger');
-        DynamicForm.find('.subject-input:not(:last) .btn-remove').html('<i class="fas fa-minus"></i>');
-    }).on('click', '.btn-add', function (e) {
-
+        DynamicForm.find('.subject-input:not(:last) .btn-remove').html('<span class="glyphicon glyphicon-minus"></span>');
+    }).on('click', '.btn-remove', function (e) {
         $(this).parents('.subject-input:first').remove();
 
         e.preventDefault();
         return false;
     });
-
 });
