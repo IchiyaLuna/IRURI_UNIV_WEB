e9ae55c084d98c7ba2dd7ccc64786b1e11d8a568
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 14:27:26 +0900

    Update calc.php

--- calc.php
+++ calc.php
@@ -61,7 +61,7 @@
                                 </div>
                             </div>
 
-                            <div class="input-group input-group-sm row g-1">
+                            <div class="row g-1">
                                 <div class="col-3">
                                     <input type="text" class="form-control" name="subject[]" placeholder="과목명">
                                 </div>
