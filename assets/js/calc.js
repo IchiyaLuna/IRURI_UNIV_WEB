f381784dec0f257e468ec1e4955497db6b133486
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 15:33:42 +0900

    Update calc.js

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -8,6 +8,7 @@ $(function () {
 
         NewEntry.find('input').val('');
         DynamicForm.find('.subject-input:not(:last) .btn-add').removeClass('btn-add').addClass('btn-remove');
+<<<<<<< HEAD
         DynamicForm.find('.subject-input:not(:last) .btn-remove').removeClass('btn-outline-success').addClass('btn-outline-danger');
         DynamicForm.find('.subject-input:not(:last) .btn-remove').html('<i class="fas fa-minus"></i>');
     }).on('click', '.btn-add', function (e) {
@@ -19,3 +20,10 @@ $(function () {
     });
 
 });
+=======
+        DynamicForm.find('.subject-input:not(:last) .btn-remove').removeClass('btn-success').addClass('btn-danger');
+        DynamicForm.find('.subject-input:not(:last) .btn-remove').html('<i class="fas fa-minus"></i>');
+    })
+
+})
+>>>>>>> parent of 42a2822 (Update calc.js)
