a99fe8025b874731e33e7ec6aed65afcdab059c1
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 14:36:26 +0900

    Update calc.php

--- calc.php
+++ calc.php
@@ -78,12 +78,14 @@
                                     <input type="number" class="form-control form-control-sm text-center" min="1" name="time[]" placeholder="0">
                                 </div>
                                 <div class="col-1 text-center">
-                                    <button type="button" class="btn btn-outline-success btn-sm">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
-                                        </svg>
-                                        <span class="visually-hidden">Button</span>
-                                    </button>
+                                    <div class="d-grid">
+                                        <button type="button" class="btn btn-outline-success btn-sm">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+                                            </svg>
+                                            <span class="visually-hidden">Button</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
