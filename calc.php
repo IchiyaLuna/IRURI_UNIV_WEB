2e6ece6105424ff02d6c65d0f4526b4bce2a7d61
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 14:16:12 +0900

    Update calc.php

--- calc.php
+++ calc.php
@@ -56,7 +56,7 @@
                                 <div class="col text-center">
                                     단위
                                 </div>
-                                <div class="col text-center">
+                                <div class="col-1 text-center">
 
                                 </div>
                             </div>
@@ -77,7 +77,7 @@
                                 <div class="col">
                                     <input type="number" class="form-control" min="1" name="time[]" placeholder="단위">
                                 </div>
-                                <div class="col">
+                                <div class="col-1">
                                     <button type="button" class="btn btn-outline-success">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
