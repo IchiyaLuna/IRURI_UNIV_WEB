bcb1e9ebb0be909db970ff690929210d45f0b79b
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 16:28:18 +0900

    Update calc.php

--- calc.php
+++ calc.php
@@ -39,30 +39,30 @@
                     <p class="lead">학생의 내신 성적을 입력하시면 내신 등급을 계산합니다.</p>
 
                     <h5 class="pb-3 mb-3 border-bottom">저런 그러나 아직 준비되지 않았습니다.</h5>
+                    <div class="dynamic-input">
+                        <form id="subject-form">
+                            <div class="container-floating">
+                                <div class="row gx-1">
+                                    <div class="col-3 text-center">
+                                        과목명
+                                    </div>
+                                    <div class="col text-center">
+                                        석차
+                                    </div>
+                                    <div class="col text-center">
+                                        동석차
+                                    </div>
+                                    <div class="col text-center">
+                                        이수자
+                                    </div>
+                                    <div class="col text-center">
+                                        단위
+                                    </div>
+                                    <div class="col-1 text-center">
 
-                    <form>
-                        <div class="container-floating">
-                            <div class="row gx-1">
-                                <div class="col-3 text-center">
-                                    과목명
+                                    </div>
                                 </div>
-                                <div class="col text-center">
-                                    석차
-                                </div>
-                                <div class="col text-center">
-                                    동석차
-                                </div>
-                                <div class="col text-center">
-                                    이수자
-                                </div>
-                                <div class="col text-center">
-                                    단위
-                                </div>
-                                <div class="col-1 text-center">
 
-                                </div>
-                            </div>
-                            <div class="dynamic-input">
                                 <div class="subject-input row g-1 py-1 align-items-center">
                                     <div class="col-3">
                                         <input type="text" class="form-control form-control-sm text-center" name="subject[]" placeholder="과목명">
@@ -88,102 +88,104 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-outline-warning">초기화</button>
-                            <button type="button" class="btn btn-outline-primary">등급 계산 하기</button>
-                        </div>
-                    </form>
-
-                </div>
-                <div class='content-sidebar col-md-4 d-none d-md-block sticky-md-top'>
-                    <div class="card">
-                        <div class="card-header">
-                            <h5>등급 안내</h5>
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">내신 등급 비율 안내</h5>
-                            <p class="card-text">내신 상위 퍼센트에 따른 등급 안내입니다.</p>
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">등급</th>
-                                        <th scope="col">누적비율</th>
-                                        <th scope="col">등급비율</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th class="grades-label" scope="row">1</th>
-                                        <td>~ 4%</td>
-                                        <td>4%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">2</th>
-                                        <td>~ 11%</td>
-                                        <td>7%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">3</th>
-                                        <td>~ 23%</td>
-                                        <td>12%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">4</th>
-                                        <td>~ 40%</td>
-                                        <td>17%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">5</th>
-                                        <td>~ 60%</td>
-                                        <td>20%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">6</th>
-                                        <td>~ 77%</td>
-                                        <td>17%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">7</th>
-                                        <td>~ 89%</td>
-                                        <td>12%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">8</th>
-                                        <td>~ 96%</td>
-                                        <td>7%</td>
-                                    </tr>
-                                    <tr>
-                                        <th class="grades-label" scope="row">9</th>
-                                        <td>~ 100%</td>
-                                        <td>4%</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-outline-warning">초기화</button>
+                                <button type="button" class="btn btn-outline-primary">등급 계산 하기</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-outline-warning">초기화</button>
+                        <button type="submit" class="btn btn-outline-primary" form="subject-form">등급 계산 하기</button>
+                    </div>
+                    <div class='content-sidebar col-md-4 d-none d-md-block sticky-md-top'>
+                        <div class="card">
+                            <div class="card-header">
+                                <h5>등급 안내</h5>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">내신 등급 비율 안내</h5>
+                                <p class="card-text">내신 상위 퍼센트에 따른 등급 안내입니다.</p>
+                                <table class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">등급</th>
+                                            <th scope="col">누적비율</th>
+                                            <th scope="col">등급비율</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th class="grades-label" scope="row">1</th>
+                                            <td>~ 4%</td>
+                                            <td>4%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">2</th>
+                                            <td>~ 11%</td>
+                                            <td>7%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">3</th>
+                                            <td>~ 23%</td>
+                                            <td>12%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">4</th>
+                                            <td>~ 40%</td>
+                                            <td>17%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">5</th>
+                                            <td>~ 60%</td>
+                                            <td>20%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">6</th>
+                                            <td>~ 77%</td>
+                                            <td>17%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">7</th>
+                                            <td>~ 89%</td>
+                                            <td>12%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">8</th>
+                                            <td>~ 96%</td>
+                                            <td>7%</td>
+                                        </tr>
+                                        <tr>
+                                            <th class="grades-label" scope="row">9</th>
+                                            <td>~ 100%</td>
+                                            <td>4%</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
-        <!-- Alert Modal -->
-        <div class="alert-modal modal fade" id="no-input" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="no-input-label">오류</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        최소 하나 이상의 등급을 입력해주세요.
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" onclick="close_modal()">확인</button>
+            <!-- Alert Modal -->
+            <div class="alert-modal modal fade" id="no-input" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="no-input-label">오류</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            최소 하나 이상의 등급을 입력해주세요.
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" onclick="close_modal()">확인</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
     </main>
 
