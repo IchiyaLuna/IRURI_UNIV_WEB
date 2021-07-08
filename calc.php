aca1fa2ffa1db62ab1fc480a7f9b4015d37f0641
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-08 16:22:05 +0900

    Update calc.php

--- calc.php
+++ calc.php
@@ -88,82 +88,82 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-outline-warning">초기화</button>
-                                <button type="button" class="btn btn-outline-primary">등급 계산 하기</button>
-                            </div>
+                        </div>
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-outline-warning">초기화</button>
+                            <button type="button" class="btn btn-outline-primary">등급 계산 하기</button>
                         </div>
                     </form>
+
+                </div>
+                <div class='content-sidebar col-md-4 d-none d-md-block sticky-md-top'>
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>등급 안내</h5>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">내신 등급 비율 안내</h5>
+                            <p class="card-text">내신 상위 퍼센트에 따른 등급 안내입니다.</p>
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">등급</th>
+                                        <th scope="col">누적비율</th>
+                                        <th scope="col">등급비율</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th class="grades-label" scope="row">1</th>
+                                        <td>~ 4%</td>
+                                        <td>4%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">2</th>
+                                        <td>~ 11%</td>
+                                        <td>7%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">3</th>
+                                        <td>~ 23%</td>
+                                        <td>12%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">4</th>
+                                        <td>~ 40%</td>
+                                        <td>17%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">5</th>
+                                        <td>~ 60%</td>
+                                        <td>20%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">6</th>
+                                        <td>~ 77%</td>
+                                        <td>17%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">7</th>
+                                        <td>~ 89%</td>
+                                        <td>12%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">8</th>
+                                        <td>~ 96%</td>
+                                        <td>7%</td>
+                                    </tr>
+                                    <tr>
+                                        <th class="grades-label" scope="row">9</th>
+                                        <td>~ 100%</td>
+                                        <td>4%</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class='content-sidebar col-md-4 d-none d-md-block sticky-md-top'>
-                <div class="card">
-                    <div class="card-header">
-                        <h5>등급 안내</h5>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">내신 등급 비율 안내</h5>
-                        <p class="card-text">내신 상위 퍼센트에 따른 등급 안내입니다.</p>
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">등급</th>
-                                    <th scope="col">누적비율</th>
-                                    <th scope="col">등급비율</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th class="grades-label" scope="row">1</th>
-                                    <td>~ 4%</td>
-                                    <td>4%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">2</th>
-                                    <td>~ 11%</td>
-                                    <td>7%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">3</th>
-                                    <td>~ 23%</td>
-                                    <td>12%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">4</th>
-                                    <td>~ 40%</td>
-                                    <td>17%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">5</th>
-                                    <td>~ 60%</td>
-                                    <td>20%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">6</th>
-                                    <td>~ 77%</td>
-                                    <td>17%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">7</th>
-                                    <td>~ 89%</td>
-                                    <td>12%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">8</th>
-                                    <td>~ 96%</td>
-                                    <td>7%</td>
-                                </tr>
-                                <tr>
-                                    <th class="grades-label" scope="row">9</th>
-                                    <td>~ 100%</td>
-                                    <td>4%</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
         </div>
 
 
