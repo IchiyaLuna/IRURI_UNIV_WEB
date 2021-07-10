951834d2501ee442725e05b25c538035882772cb
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-11 06:25:24 +0900

    mb-3

--- module/jumbo_select.php
+++ module/jumbo_select.php
@@ -1,13 +1,13 @@
 <div class="collapse multi-collapse-univ multi-collapse-dept show">
-    <div class="row gy-3 align-items-md-stretch">
-        <div class="col-md-6">
+    <div class="row align-items-md-stretch">
+        <div class="col-md-6 mb-3">
             <div class="h-100 p-5 text-white bg-dark border rounded-3 position-relative">
                 <h2>대학교 기준</h2>
                 <p>입력한 내신과 정시 등급을 기준으로 전체 대학교 목록에 합격 가능성과 학생의 각 대학별 환산 점수를 표시하고, 추가적인 정보도 제공합니다.</p>
                 <button class="btn btn-primary stretched-link" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse-univ">학교 보러가기</button>
             </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 mb-3">
             <div class="h-100 p-5 bg-light border rounded-3 position-relative">
                 <h2>학과 기준</h2>
                 <p>원하는 학과의 분야를 선택하면 해당 분야를 기준으로 현재 가능성 있는 학교의 학과들과 세부 정보를 출력합니다.</p>
