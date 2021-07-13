9cb6009177fb74b89fa1fd486a53c89b5e70f02a
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-13 18:18:12 +0900

    Update result_calc.php

--- result_calc.php
+++ result_calc.php
@@ -60,12 +60,12 @@
 
                                     echo "<tr>";
 
-                                    echo "<td>" . $data[0] . "</td>";
-                                    echo "<td>" . $data[1] . "</td>";
-                                    echo "<td>" . $data[2] . "</td>";
-                                    echo "<td>" . $data[3] . "</td>";
-                                    echo "<td>" . $data[4] . "</td>";
-                                    echo "<td>" . $data[5] . "</td>";
+                                    echo "<td>" . $data[0] . "등" . "</td>";
+                                    echo "<td>" . $data[1] . "명" . "</td>";
+                                    echo "<td>" . $data[2] . "명" . "</td>";
+                                    echo "<td>" . $data[3] . "명" . "</td>";
+                                    echo "<td>" . $data[4] . "단위" . "</td>";
+                                    echo "<td class='text-danger'>" . $data[5] . "등급" . "</td>";
 
                                     echo "</tr>";
                                 }
