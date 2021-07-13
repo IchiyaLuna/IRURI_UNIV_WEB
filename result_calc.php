97d03ee6593d07dc6018f6c043a16b19db0a693e
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-13 17:44:42 +0900

    Update result_calc.php

--- result_calc.php
+++ result_calc.php
@@ -60,12 +60,12 @@
 
                                     echo "<tr>";
 
-                                    echo "<td>" . $result[0] . "</td>";
-                                    echo "<td>" . $result[1] . "</td>";
-                                    echo "<td>" . $result[2] . "</td>";
-                                    echo "<td>" . $result[3] . "</td>";
-                                    echo "<td>" . $result[4] . "</td>";
-                                    echo "<td>" . $result[5] . "</td>";
+                                    echo "<td>" . $data[0] . "</td>";
+                                    echo "<td>" . $data[1] . "</td>";
+                                    echo "<td>" . $data[2] . "</td>";
+                                    echo "<td>" . $data[3] . "</td>";
+                                    echo "<td>" . $data[4] . "</td>";
+                                    echo "<td>" . $data[5] . "</td>";
 
                                     echo "</tr>";
                                 }
