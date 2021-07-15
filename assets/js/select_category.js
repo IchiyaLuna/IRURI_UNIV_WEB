ec1a9e0f495680e81e59aef36a751a3ed73b6522
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-15 22:15:22 +0900

    Update select_category.js

--- assets/js/select_category.js
+++ assets/js/select_category.js
@@ -2,8 +2,8 @@ var dept = false;
 
 function update_selected() {
     $("#detail").val(0);
-    $("#detail").find("option[value!=-1]").detach();
-
+    $("#detail").find("option[value!=-2]").detach();
+    $('#detail').append("<option value='-1' selected>세부 분류를 선택해주세요</select>")
     $("#detail").append(dept.filter("." + $(this).val()));
 }
 
