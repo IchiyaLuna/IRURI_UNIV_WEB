c6a615ec1fbc1abd4d8f0f2accb9d940a3a7396b
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-18 14:27:35 +0900

    Update admin.php

--- admin.php
+++ admin.php
@@ -24,7 +24,7 @@
         <title>UNDER DEV - Admin Page</title>
 
         <!-- Custom fonts for this template-->
-        <link href="./assets/fa-assets/all.min.css" rel="stylesheet" type="text/css">
+        <link href="./assets/fa-assets/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
         <!-- Custom styles for this template-->
