38ac94bfff7d028c367f46e04fb1030de05c56bb
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-27 13:51:21 +0900

    js fix

--- assets/js/jungshi_time.js
+++ assets/js/jungshi_time.js
@@ -1,16 +1,16 @@
-var dept = false;
+var month = false;
 
-function update_selected() {
+function update_selected_ym() {
     $("#month").val(0);
     $("#month").find("option[value!=-2]").detach();
     $('#month').append("<option value='-1' selected>세부 분류를 선택해주세요</select>")
-    $("#month").append(dept.filter("." + $(this).val()));
+    $("#month").append(month.filter("." + $(this).val()));
 }
 
 $(function () {
-    dept = $("#month").find("option[value!=-1]");
-    dept.detach();
+    month = $("#month").find("option[value!=-1]");
+    month.detach();
 
-    $("#year").change(update_selected);
+    $("#year").change(update_selected_ym);
     $("#year").trigger("change");
 });
