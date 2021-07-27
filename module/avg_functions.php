6e2b75c997e6db60ca3c2d0f9e84acefe090bd7f
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-07-28 08:19:24 +0900

    Update avg_functions.php

--- module/avg_functions.php
+++ module/avg_functions.php
@@ -6,16 +6,17 @@ function white($arr)
     $valid = 0;
     $index = 0;
     $offsets = array(1, 1, 1);
+    $sum = 0;
 
     foreach ($offsets as $offset) {
         if ($arr[$index] != 0) {
-            $arr[$index] *= $offset;
+            $sum = $arr[$index] * $offset;
             $valid += $offset;
         }
         $index++;
     }
 
-    return array_sum($arr) / $valid;
+    return $sum / $valid;
 }
 
 function yellow($arr)
@@ -24,16 +25,17 @@ function yellow($arr)
     $valid = 0;
     $index = 0;
     $offsets = array(30, 35, 35);
+    $sum = 0;
 
     foreach ($offsets as $offset) {
         if ($arr[$index] != 0) {
-            $arr[$index] *= $offset;
+            $sum = $arr[$index] * $offset;
             $valid += $offset;
         }
         $index++;
     }
 
-    return array_sum($arr) / $valid;
+    return $sum / $valid;
 }
 
 function blue($arr)
@@ -42,16 +44,17 @@ function blue($arr)
     $valid = 0;
     $index = 0;
     $offsets = array(3, 4, 3);
+    $sum = 0;
 
     foreach ($offsets as $offset) {
         if ($arr[$index] != 0) {
-            $arr[$index] *= $offset;
+            $sum = $arr[$index] * $offset;
             $valid += $offset;
         }
         $index++;
     }
 
-    return array_sum($arr) / $valid;
+    return $sum / $valid;
 }
 
 function purple($arr)
@@ -60,14 +63,15 @@ function purple($arr)
     $valid = 0;
     $index = 0;
     $offsets = array(2, 4, 4);
+    $sum = 0;
 
     foreach ($offsets as $offset) {
         if ($arr[$index] != 0) {
-            $arr[$index] *= $offset;
+            $sum = $arr[$index] * $offset;
             $valid += $offset;
         }
         $index++;
     }
 
-    return array_sum($arr) / $valid;
+    return $sum / $valid;
 }
