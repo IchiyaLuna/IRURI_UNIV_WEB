cd0d97e3f29cbb062f5566cfb4ba391d3f0cf9cf
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-08-03 04:28:41 +0900

    Update admin.php

--- admin.php
+++ admin.php
@@ -207,7 +207,7 @@
                                 <div class="card shadow mb-4">
                                     <!-- Card Header - Dropdown -->
                                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                        <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                                        <h6 class="m-0 font-weight-bold text-primary">최근 5일 사용자 이용 기록</h6>
                                     </div>
                                     <!-- Card Body -->
                                     <div class="card-body">
@@ -223,34 +223,19 @@
                                 <div class="card shadow mb-4">
                                     <!-- Card Header - Dropdown -->
                                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                        <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                                        <div class="dropdown no-arrow">
-                                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                            </a>
-                                            <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                                                <div class="dropdown-header">Dropdown Header:</div>
-                                                <a class="dropdown-item" href="#">Action</a>
-                                                <a class="dropdown-item" href="#">Another action</a>
-                                                <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item" href="#">Something else here</a>
-                                            </div>
-                                        </div>
+                                        <h6 class="m-0 font-weight-bold text-primary">기록 종류</h6>
                                     </div>
                                     <!-- Card Body -->
                                     <div class="card-body">
                                         <div class="chart-pie pt-4 pb-2">
-                                            <canvas id="myPieChart"></canvas>
+                                            <canvas id="typepie"></canvas>
                                         </div>
                                         <div class="mt-4 text-center small">
                                             <span class="mr-2">
-                                                <i class="fas fa-circle text-primary"></i> Direct
+                                                <i class="fas fa-circle text-primary"></i> 학교 검색
                                             </span>
                                             <span class="mr-2">
-                                                <i class="fas fa-circle text-success"></i> Social
-                                            </span>
-                                            <span class="mr-2">
-                                                <i class="fas fa-circle text-info"></i> Referral
+                                                <i class="fas fa-circle text-success"></i> 학과 검색
                                             </span>
                                         </div>
                                     </div>
@@ -363,6 +348,8 @@
         $today = date('Y-m-d', $_SERVER['REQUEST_TIME']);
 
         $logs = array(0, 0, 0, 0, 0);
+        $ucount = 0;
+        $dcount = 0;
 
         foreach ($uloglist as $log) {
             if ($log['time'] == $four) {
@@ -376,6 +363,8 @@
             } else if ($log['time'] == $today) {
                 $logs[4]++;
             }
+
+            $ucount++;
         }
 
         foreach ($dloglist as $log) {
@@ -390,6 +379,8 @@
             } else if ($log['time'] == $today) {
                 $logs[4]++;
             }
+
+            $dcount++;
         }
         ?>
 
@@ -514,6 +505,38 @@
             });
         </script>
 
+        <script>
+            var ctx = document.getElementById("typepie");
+            var tpie = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: ["학교 검색", "학과 검색"],
+                    datasets: [{
+                        data: [<?php echo $ucount; ?>, <?php echo $ucount; ?>],
+                        backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                        hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                        hoverBorderColor: "rgba(234, 236, 244, 1)",
+                    }],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    tooltips: {
+                        backgroundColor: "rgb(255,255,255)",
+                        bodyFontColor: "#858796",
+                        borderColor: '#dddfeb',
+                        borderWidth: 1,
+                        xPadding: 15,
+                        yPadding: 15,
+                        displayColors: false,
+                        caretPadding: 10,
+                    },
+                    legend: {
+                        display: false
+                    },
+                    cutoutPercentage: 80,
+                },
+            });
+        </script>
     </body>
 
     </html>
