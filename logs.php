2eaccf4344f2a01e8a5a454553cccd83093fd04f
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-08-03 05:36:25 +0900

    Update logs.php

--- logs.php
+++ logs.php
@@ -443,7 +443,7 @@
 
         <!-- Bootstrap core JavaScript-->
         <script src="./assets/vendor/jquery/jquery.min.js"></script>
-        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
         <!-- Core plugin JavaScript-->
         <script src="./assets/vendor/jquery-easing/jquery.easing.min.js"></script>
