8e8a59b508f37451bedd81666cb96cf258f98e03
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-08-02 22:57:55 +0900

    fix

--- assets/js/jungshi_time.js
+++ assets/js/jungshi_time.js
@@ -1,26 +1,27 @@
-var month = false;
+var umonth = false;
+var dmonth = false;
 
 function update_selected_ym() {
     $("#univ-month").val(0);
     $("#univ-month").find("option[value!=-2]").detach();
     $('#univ-month').append("<option value='-1' selected>응시하지 않음</select>")
-    $("#univ-month").append(month.filter("." + $(this).val()));
+    $("#univ-month").append(umonth.filter("." + $(this).val()));
 
     $("#dept-month").val(0);
     $("#dept-month").find("option[value!=-2]").detach();
     $('#dept-month').append("<option value='-1' selected>응시하지 않음</select>")
-    $("#dept-month").append(month.filter("." + $(this).val()));
+    $("#dept-month").append(dmonth.filter("." + $(this).val()));
 }
 
 $(function () {
-    month = $("#univ-month").find("option[value!=-1]");
-    month.detach();
+    umonth = $("#univ-month").find("option[value!=-1]");
+    umonth.detach();
 
     $("#univ-year").change(update_selected_ym);
     $("#univ-year").trigger("change");
 
-    month = $("#dept-month").find("option[value!=-1]");
-    month.detach();
+    dmonth = $("#dept-month").find("option[value!=-1]");
+    dmonth.detach();
 
     $("#dept-year").change(update_selected_ym);
     $("#dept-year").trigger("change");
