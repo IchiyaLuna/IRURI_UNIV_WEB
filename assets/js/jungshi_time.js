7c2fbeaba2047ea14f30a1e1ca2ca56200812c3d
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-08-02 23:15:10 +0900

    Update jungshi_time.js

--- assets/js/jungshi_time.js
+++ assets/js/jungshi_time.js
@@ -1,12 +1,14 @@
 var umonth = false;
 var dmonth = false;
 
-function update_selected_ym() {
+function update_selected_ym_u() {
     $("#univ-month").val(0);
     $("#univ-month").find("option[value!=-2]").detach();
     $('#univ-month').append("<option value='-1' selected>응시하지 않음</select>")
     $("#univ-month").append(umonth.filter("." + $(this).val()));
+}
 
+function update_selected_ym_d() {
     $("#dept-month").val(0);
     $("#dept-month").find("option[value!=-2]").detach();
     $('#dept-month').append("<option value='-1' selected>응시하지 않음</select>")
@@ -17,12 +19,12 @@ $(function () {
     umonth = $("#univ-month").find("option[value!=-1]");
     umonth.detach();
 
-    $("#univ-year").change(update_selected_ym);
+    $("#univ-year").change(update_selected_ym_u);
     $("#univ-year").trigger("change");
 
     dmonth = $("#dept-month").find("option[value!=-1]");
     dmonth.detach();
 
-    $("#dept-year").change(update_selected_ym);
+    $("#dept-year").change(update_selected_ym_d);
     $("#dept-year").trigger("change");
 });
