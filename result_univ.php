a7d85aa2be4bd4870f71554c853c34d10c856880
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-08-21 22:20:58 +0900

    Update result_univ.php

--- result_univ.php
+++ result_univ.php
@@ -565,9 +565,21 @@ if (isset($_COOKIE['authid'])) {
     <script src="./assets/js/jquery-3.6.0.min.js"></script>
 
     <script>
-        function getCookie(name) {
-            var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-            return value ? value[2] : null;
+        function getCookie(key) {
+            var result = null;
+            var cookie = document.cookie.split(';');
+            cookie.some(function(item) {
+                // 공백을 제거
+                item = item.replace(' ', '');
+
+                var dic = item.split('=');
+
+                if (key === dic[0]) {
+                    result = dic[1];
+                    return true; // break;
+                }
+            });
+            return result;
         }
 
         function openmodal(modalcode) {
