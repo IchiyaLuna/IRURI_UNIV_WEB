003e55805857b7346bf1e3bb66c393a2d1c896e9
Ichiya Luna <akkyu0724@gmail.com>
2021-10-02 17:15:39 +0900

    Update calc.php

--- calc.php
+++ calc.php
@@ -46,7 +46,7 @@
                     <div class="dynamic-input mb-3">
                         <form id="subject-form" class="needs-validation" method="POST" action="./result_calc.php" autocomplete="off" novalidate>
 
-                            <table class="table">
+                            <table class="table table-light">
                                 <thead>
                                     <th scope="col">
                                         <div class="text-center">학년</div>
