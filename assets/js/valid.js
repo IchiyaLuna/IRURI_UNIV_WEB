654078617999fe3c7859668c674fe7972ab4bc40
Ichiya Luna <47956845+akkyu0724@users.noreply.github.com>
2021-10-04 18:59:35 +0900

    Update valid.js

--- assets/js/valid.js
+++ assets/js/valid.js
@@ -2,7 +2,7 @@ function count_arr(arr) {
     var count = 0;
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] != "") {
+        if (arr[i].value != "") {
             count++;
         }
     }
